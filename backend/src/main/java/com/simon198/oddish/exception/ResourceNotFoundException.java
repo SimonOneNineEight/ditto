ce6add2a88ff0b701ef17280b127c0cb8f687279
SimonOneNineEight <ss121549@gmail.com>
2024-12-10 23:51:55 -0800

    init express.js

--- backend/src/main/java/com/simon198/oddish/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.simon198.oddish.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-	public ResourceNotFoundException(String message) {
-		super(message);
-	}
-}
