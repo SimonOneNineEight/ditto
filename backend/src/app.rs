baedabc01da0c462a44194dbd11bcdd0def1ef17
SimonOneNineEight <ss121549@gmail.com>
2025-02-25 16:55:43 -0800

    Add rate limiter to all route

--- backend/src/app.rs
+++ backend/src/app.rs
@@ -1,12 +1,21 @@
 use std::sync::Arc;
 
 use axum::Router;
+use tower_governor::{governor::GovernorConfigBuilder, GovernorLayer};
 use tower_http::cors::{Any, CorsLayer};
 
 use crate::{routes, utils::state::AppState};
 
 pub fn create_app(app_state: Arc<AppState>) -> Router {
+    let config = Arc::new(
+        GovernorConfigBuilder::default()
+            .per_second(5)
+            .burst_size(10)
+            .finish()
+            .unwrap(),
+    );
     Router::new()
-        .merge(routes::register_routes(app_state.clone()))
+        .layer(GovernorLayer { config })
         .layer(CorsLayer::new().allow_origin(Any))
+        .merge(routes::register_routes(app_state.clone()))
 }
