d5585d679f3c24c7efb775fbeb64005c35e2714b
SimonOneNineEight <ss121549@gmail.com>
2025-03-03 15:08:10 -0800

    add auth context

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -10,30 +10,25 @@ import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardFooter } from "@/components/ui/card"
 import { useAuth } from "@/contexts/AuthContext"
 
-const registerSchema = z.object({
-    name: z.string().min(2, "Name is required"),
+const loginSchema = z.object({
     email: z.string().email("Invalid email address"),
     password: z.string().min(8, "Password must be at least 8 characters"),
-    confirmPassword: z.string().min(8, "Confirm password is required")
-}).refine(data => data.password === data.confirmPassword, {
-    message: "Passwords do not match",
-    path: ["confirmPassword"]
 });
 
-type RegisterFormData = z.infer<typeof registerSchema>;
+type LoginFormData = z.infer<typeof loginSchema>;
 
 const page = () => {
     const {
         register,
         handleSubmit,
         formState: { errors }
-    } = useForm<RegisterFormData>({ resolver: zodResolver(registerSchema) });
+    } = useForm<LoginFormData>({ resolver: zodResolver(loginSchema) });
 
-    const { register: registerUser } = useAuth();
+    const { login } = useAuth();
 
-    const onSubmit = async (data: RegisterFormData) => {
+    const onSubmit = async (data: LoginFormData) => {
         try {
-            await registerUser(data.name, data.email, data.password);
+            await login(data.email, data.password);
         } catch (error) {
             console.error(error);
         }
@@ -41,24 +36,13 @@ const page = () => {
 
     return (
         <div className="p-4 flex flex-col gap-8 row-start-2 items-center mx-8">
-            <h1 className="text-2xl pt-6 font-semibold">Register to Ditto</h1>
+            <h1 className="text-2xl pt-6 font-semibold">Welcome Back</h1>
             <Card className="w-[21rem]">
                 <form
                     onSubmit={handleSubmit(onSubmit)}
                 >
                     <CardContent className="p-6">
                         <div className="grid w-full items-center gap-2">
-                            <div className="w-full">
-                                <label className="block">
-                                    Name
-                                </label>
-                                <input
-                                    {...register("name")}
-                                    className="w-full mt-1 px-2 py-1  outline-gray-600 border-b-2 border-b-gray-300"
-                                />
-                                {errors.name ? <p className={"text-red-500 text-sm"}>{errors.name.message}</p> : <p className={"h-5"}></p>}
-                            </div>
-
                             <div className=" w-full">
                                 <label className="block">
                                     Email
@@ -81,23 +65,12 @@ const page = () => {
                                 />
                                 {errors.password ? <p className={"text-red-500 text-sm"}>{errors.password.message}</p> : <p className={"h-5"}></p>}
                             </div>
-                            <div className="w-full mb-4">
-                                <label className="block">
-                                    Confirm Password
-                                </label>
-                                <input
-                                    type="password"
-                                    {...register("confirmPassword")}
-                                    className="w-full mt-1 px-2 py-1  outline-gray-600 border-b-2 border-b-gray-300"
-                                />
-                                {errors.confirmPassword ? <p className={"text-red-500 text-sm"}>{errors.confirmPassword.message}</p> : <p className={"h-5"}></p>}
-                            </div>
                         </div>
                         <CardFooter className="p-0 pb-4">
                             <Button
                                 type="submit"
                                 className="w-full bg-sky-600 text-white font-bold">
-                                Register
+                                Login
                             </Button>
                         </CardFooter>
                     </CardContent>
--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -1,5 +1,5 @@
 import api from '@/lib/axios';
-import { LoginResponse } from '@/types/authType';
+import { LoginResponse, UserResponse } from '@/types/authType';
 
 export const authService = {
     async register(
@@ -17,6 +17,43 @@ export const authService = {
             return data;
         } catch (error) {
             console.error('Error registering user: ', error);
+            throw error;
+        }
+    },
+
+    async login(email: string, password: string): Promise<LoginResponse> {
+        try {
+            const { data } = await api.post('/api/login', {
+                email,
+                password,
+            });
+
+            return data;
+        } catch (error) {
+            console.error('Error logging in: ', error);
+            throw error;
+        }
+    },
+
+    async refreshToken(refreshToken: string): Promise<LoginResponse> {
+        try {
+            const { data } = await api.post('/api/refresh_token', {
+                refresh_token: refreshToken,
+            });
+            return data.data;
+        } catch (error) {
+            console.error('Error refreshing token: ', error);
+            throw error;
+        }
+    },
+
+    async getMe(): Promise<UserResponse> {
+        try {
+            const { data } = await api.get('/api/me');
+
+            return data;
+        } catch (error) {
+            console.error('Error fetching user: ', error);
             throw error;
         }
     },
