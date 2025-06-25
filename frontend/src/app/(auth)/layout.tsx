90c1e1aa0fc84973768ed006c0101fe8a33351c0
SimonOneNineEight <ss121549@gmail.com>
2025-06-24 21:50:15 -0400

    refactor login page

--- frontend/src/app/(auth)/layout.tsx
+++ frontend/src/app/(auth)/layout.tsx
@@ -1,11 +1,19 @@
 import type { Metadata } from 'next';
 import { ThemeProvider } from '@/components';
 import { AuthProvider } from '@/contexts/AuthContext';
+import Link from 'next/link';
+import { Suez_One } from 'next/font/google';
 
 export const metadata: Metadata = {
     title: 'Ditto - Authentication',
     description: 'Login or register for Ditto job tracker',
 };
+
+const suezOne = Suez_One({
+    weight: '400',
+    subsets: ['latin'],
+    display: 'swap',
+});
 
 export default function AuthLayout({
     children,
@@ -20,10 +28,16 @@ export default function AuthLayout({
             disableTransitionOnChange
         >
             <AuthProvider>
-                <div className="min-h-screen flex items-center justify-center bg-gray-50 dark:bg-gray-900">
-                    {children}
+                <div className="h-dvh grid grid-rows-[auto_1fr]">
+                    <Link href="/" className="block m-4">
+                        <h1 className={suezOne.className}>Ditto</h1>
+                    </Link>
+                    <div className="flex items-center justify-center bg-background">
+                        {children}
+                    </div>
                 </div>
             </AuthProvider>
         </ThemeProvider>
     );
 }
+
